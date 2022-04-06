37c613a057c7a3b5703742c025b712aedc3fc3d8
Spencer Johnson <sljohnson2222@gmail.com>
2022-04-06 16:30:39 -0500

    commit tree 5504ad78563c697e3b6df0ae0f069849b8ebfd45

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     items = Item.all
     render json: items, include: :user
   end
 
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
